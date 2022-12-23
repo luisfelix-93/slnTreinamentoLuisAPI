e9e749e957abb87b85e087e22ce5b2465ee2d343
Luis Felipe Félix Filho <luis.felix.filho@hotmail.com>
2022-12-23 11:52:22 -0300

    Add project files.

---
+++ TreinamentoLuisAPI/Controllers/TokenController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TreinamentoLuisAPI.Models;
+using TreinamentoLuisAPI.Repositories;
+using TreinamentoLuisAPI.Services;
+
+namespace TreinamentoLuisAPI.Controllers
+{
+    public class TokenController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/login")]
+        [AllowAnonymous]
+
+        public ActionResult<dynamic> Autenticate([FromBody] UserAPI model)
+        {
+            var user = UserRepository.GetUser(model.NmUser, model.DePassword);
+            if (user == null)
+            {
+                return NotFound(new { message = "Usuario não encontrado" });
+            }
+            var token = TokenServices.GenerateToken(user);
+            user.DePassword = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ TreinamentoLuisAPI/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TreinamentoLuisAPI.Models;
+
+namespace TreinamentoLuisAPI.Repositories
+{
+    public class UserRepository
+    {
+        public static UserAPI GetUser(string pNameUser, string pDePassword)
+        {
+            var users = new List<UserAPI>();
+            users.Add(new UserAPI { NmUser = "Luis", DePassword = "felix@123" });
+
+            return users.FirstOrDefault(x => x.NmUser.ToLower() == pNameUser.ToLower() && x.DePassword == pDePassword);
+        }
+    }
+}
